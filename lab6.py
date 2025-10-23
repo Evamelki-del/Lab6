bff1eec20d26ca1e529a3e36a34790f6f83897a7
Evamelki-del <Evamelki@icloud.com>
2025-10-23 15:09:26 -0400

    Evas contribution, part 3

--- lab6.py
+++ lab6.py
@@ -6,3 +6,31 @@ import pandas as pd
 
 
 data = pd.read_csv("wdi_wide.csv")
+
+#Part 3
+
+
+
+#Question 4
+#Printing the amount of unique values per column using nunique function
+
+print(data.nunique())
+
+#Question 6
+
+#Adding the "GNI per capita" column 
+#and rounding to the nearest cent (2 decimal places) in the same function
+
+data["GNI per capita"] = round(data["GNI"] / data ["Population"], 2)
+
+#Question 8
+#Relationship between the high income economies and their region
+
+HIE_vs_region = pd.crosstab(data["High Income Economy"], data["Region"])
+
+print("The high income economies are in: ")
+
+row = HIE_vs_region.iloc[1]
+cols = row[row != 0].index   
+print(list(cols)) 
+
